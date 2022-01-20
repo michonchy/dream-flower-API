5dc8828c068ace5068759bd8cd2f0eaac3c00ec4
michonchy <mintan0222@gmail.com>
2022-01-21 08:32:46 +0900

    夢想花

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,18 +1,10 @@
+import json
+
+import pytest
+
+from dream_number import app
 
 
-
-def is_dream_flower(n):
-    dream_flower = ""
-    for a in range(n):
-        music = ""
-        for i in range (13):
-            if i <= 8:
-                music += "とんで"
-            elif 8 < i <= 11:
-                music += "まわって"
-            else:
-                music += "まわる"
-        dream_flower += music + "\n"
-    return dream_flower
-
-print(is_dream_flower(3))
+def test_is_dream_flower():
+    assert app.is_dream_flower(1) == '''とんでとんでとんでとんでとんでとんでとんでとんでとんでまわってまわってまわってまわる
+'''
